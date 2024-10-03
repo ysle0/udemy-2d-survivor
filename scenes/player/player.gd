89004acc3e114ca13af795b3c9b0635f7ec4fc2a
yoonsang Lee <ysle@pm.me>
2024-10-04 03:20:24 +0900

    add: player, player movement and main map w tilemap

---
+++ scenes/player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const MAX_MOVEMENT_SPEED = 200
+
+func _ready():
+	pass
+
+
+func _process(delta: float):
+	var movement_vec = get_movement_vec2()
+	var movement_dir = movement_vec.normalized()
+	velocity = movement_dir * MAX_MOVEMENT_SPEED
+
+	move_and_slide()
+
+	pass
+
+
+func get_movement_vec2():
+	var move_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var move_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(move_x, move_y)

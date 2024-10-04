eca30780b14bc626c4361f1bb6c074d79a41e191
yoonsang lee <highp0912@pm.me>
2024-10-05 00:46:41 +0900

    refac

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,16 +1,15 @@
 extends CharacterBody2D
 
-const MAX_MOVEMENT_SPEED = 200
+const MAX_MOVEMENT_SPEED := 200
 
 
-func _physics_process(_delta: float):
-	var movement_vec = get_movement_vec2()
-	var movement_dir = movement_vec.normalized()
-	velocity = movement_dir * MAX_MOVEMENT_SPEED
+func _physics_process(delta: float):
+	var movement_dir := get_movement_vec2().normalized()
+	self.velocity = movement_dir * MAX_MOVEMENT_SPEED
 	move_and_slide()
 
 
-func get_movement_vec2():
-	var move_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var move_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+func get_movement_vec2() -> Vector2:
+	var move_x := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var move_y := Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(move_x, move_y)

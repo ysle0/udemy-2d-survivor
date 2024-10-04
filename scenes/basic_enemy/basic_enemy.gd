9b4f0b2d2c89d836a7d10d6e9e43e2127f04de28
yoonsang lee <highp0912@pm.me>
2024-10-04 14:27:31 +0900

    add: basic_enemy

---
+++ scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 75
+const MIN_DISTANCE_TO_PLAYER = 4.5 * 4.5
+var player: Node2D
+
+func _ready():
+	var tree = get_tree()
+	if !tree.has_group("player"):
+		push_error("err: no player group found")
+		return
+
+	self.player = get_tree().get_first_node_in_group("player")
+
+
+func _physics_process(delta: float):
+	if !self.check_distance_between_player(MIN_DISTANCE_TO_PLAYER):
+		return
+
+	var dir_to_player = self.get_direction_to_player()
+	velocity = dir_to_player * MAX_SPEED
+	move_and_slide()
+
+
+func get_direction_to_player() -> Vector2:
+	if self.player != null:
+		return (self.player.global_position - global_position).normalized()
+	return Vector2.ZERO
+
+
+func check_distance_between_player(min_distance: float) -> bool:
+	var distance_to_player = global_position.distance_to(self.player.global_position)
+	print("distance to player: %s" % distance_to_player)
+	return distance_to_player > min_distance

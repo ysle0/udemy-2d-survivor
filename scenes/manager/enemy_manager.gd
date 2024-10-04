b1ab13fd4099c1a434fe36c14f7d11f154014825
yoonsang lee <highp0912@pm.me>
2024-10-05 01:24:29 +0900

    add: enemy manager

---
+++ scenes/manager/enemy_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+const SPAWN_RADIUS := 330
+
+@export var spawn_interval: float = 1.0
+@export var basic_enemy_scene: PackedScene
+
+func _ready():
+	assert(basic_enemy_scene != null)
+	$Timer.timeout.connect(self.on_timer_timeout)
+
+
+func on_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player")
+	if player == null:
+		return
+
+	var rnd_dir := Vector2.UP.rotated(randf_range(0, TAU))
+	var rnd_pos_in_circle := rnd_dir * SPAWN_RADIUS
+	var spawn_pos: Vector2 = player.global_position + rnd_pos_in_circle
+
+	var enemy := self.basic_enemy_scene.instantiate() as Node2D
+	get_parent().add_child(enemy)
+	enemy.global_position = spawn_pos

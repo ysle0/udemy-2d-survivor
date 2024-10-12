0bb538359a2461754ed5769f38699d86b22d4ed6
yoonsang lee <highp0912@pm.me>
2024-10-12 21:29:47 +0900

    add: sword ability upgrade

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -3,6 +3,7 @@ class_name ExperienceManager
 
 
 signal experience_updated(current_experience: float, target_experience: float)
+signal level_up(new_level: int)
 
 const TARGET_EXPERIENCE_GROWTH := 1.5
 
@@ -21,10 +22,14 @@ func increment_experience(amount: float):
     print("cur exp: %f" % current_experience)
 
     if current_experience >= target_experience:
-        current_level += 1
+        # update experience
         target_experience *= TARGET_EXPERIENCE_GROWTH
         current_experience = 0.0
         experience_updated.emit(current_experience, target_experience)
+
+        # update level
+        current_level += 1
+        level_up.emit(current_level)
 
 
 func on_experience_vial_collected(amount: float):
---
+++ scenes/manager/upgrade_manager.gd
@@ -0,0 +1,30 @@
+extends Node
+class_name UpgradeManager
+
+@export var upgrade_pool: Array[AbilityUpgrade]
+@export var experience_manager: ExperienceManager
+
+var current_upgrades := {
+    "test": "hello"
+}
+
+
+func _ready():
+    experience_manager.level_up.connect(on_level_up)
+
+
+func on_level_up(new_level: int):
+    var chosen_upgrade := upgrade_pool.pick_random() as AbilityUpgrade
+    if chosen_upgrade == null:
+        return
+    
+    var has_upgrade := current_upgrades.has(chosen_upgrade.id)
+    if not has_upgrade:
+        current_upgrades[chosen_upgrade.id] = {
+            "resource": chosen_upgrade,
+            "quantity": 1
+        }
+    else:
+        current_upgrades[chosen_upgrade.id]["quantity"] += 1
+
+    print(current_upgrades)

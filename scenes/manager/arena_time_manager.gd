7db5f3f7c0d840a55cff587c6149aff14007bdbf
yoonsang lee <highp0912@pm.me>
2024-10-16 01:38:11 +0900

    add: victory screen

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,9 +1,20 @@
 extends Node
+class_name ArenaTimeManager
+
+@export var victory_screen_scene: PackedScene
 
 @onready var timer := %Timer as Timer
+
+func _ready():
+	self.timer.timeout.connect(self.on_timer_timeout)
 
 
 func get_time_elapsed() -> float:
 	return timer.wait_time - timer.time_left
 
 
+func on_timer_timeout():
+	print("on_timer_timeout> overwinning de spel!")
+	var victory_screen_instance := \
+		self.victory_screen_scene.instantiate() as VictoryScreen
+	owner.add_child(victory_screen_instance)
---
+++ scenes/ui/victory_screen.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+class_name VictoryScreen
+
+@onready var restart_button: Button = %RestartButton
+@onready var quit_button: Button = %QuitButton
+
+func _ready() -> void:
+	get_tree().paused = true
+
+	self.restart_button.pressed.connect(on_restart_button_pressed)
+	self.quit_button.pressed.connect(on_quit_button_pressed)
+
+
+func on_restart_button_pressed() -> void:
+	print("on_restart_button_pressed")
+	var tree := get_tree()
+	tree.paused = false
+	# replace current scene with the specificed scene file
+	tree.change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func on_quit_button_pressed():
+	print("on_quit_button_pressed")
+	get_tree().quit()

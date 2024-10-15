1cb713867432232d8f3a432960513bd9bfbd4e76
yoonsang lee <highp0912@pm.me>
2024-10-16 00:41:28 +0900

    add: health bar UI / connet

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -2,7 +2,10 @@ extends Node
 class_name HealthComponent
 
 signal died
+signal health_changed
+
 @export var max_health: float = 10
+
 var current_health: float
 
 
@@ -12,7 +15,15 @@ func _ready():
 
 func take_damage(damage_amount: float):
 	current_health = maxf(current_health - damage_amount, 0.0)
+	health_changed.emit()
 	Callable(check_death).call_deferred()
+
+
+func get_health_percent() -> float:
+	if max_health <= 0.0:
+		return 0.0
+
+	return min(current_health / max_health, 1.0)
 
 
 func check_death():
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,20 +1,32 @@
 extends CharacterBody2D
+class_name Player
 
 const MAX_MOVEMENT_SPEED := 125
 const ACCELATION_SMOOTHING := 15
 
+@onready var health_component: HealthComponent = %HealthComponent
+@onready var damage_interval_timer: Timer = %DamageIntervalTimer
+@onready var collision_area_2d: Area2D = %CollisionArea2D
+@onready var dash_ability_controller: DashAbilityController = \
+	%DashAbilityController
+@onready var health_bar: ProgressBar = %HealthBar
+
 var colliding_body_count: int = 0
 
 func _ready():
-	%CollisionArea2D.body_entered.connect(on_body_entered)
-	%CollisionArea2D.body_exited.connect(on_body_exited)
-	%DamageIntervalTimer.timeout.connect(on_damage_interval_timer_timeout)
+	self.collision_area_2d.body_entered.connect(on_body_entered)
+	self.collision_area_2d.body_exited.connect(on_body_exited)
+	self.damage_interval_timer.timeout.connect(
+		on_damage_interval_timer_timeout)
+	self.health_component.health_changed.connect(on_health_changed)
+	self.update_health_ui()
 
 
 func _process(delta: float):
 	var movement_dir := get_movement_vec2().normalized()
 	var target_velocity := movement_dir * self.MAX_MOVEMENT_SPEED
-	target_velocity = %DashAbilityController.apply_dash_power(target_velocity)
+	target_velocity = self.dash_ability_controller \
+		.apply_dash_power(target_velocity)
 
 	var weight := 1 - exp(-delta * self.ACCELATION_SMOOTHING)
 	var next_velocity := self.velocity.lerp(target_velocity, weight)
@@ -28,7 +40,7 @@ func get_movement_vec2() -> Vector2:
 		Input.get_action_strength("move_right") \
 	  - Input.get_action_strength("move_left")
 	var move_y := \
-	    Input.get_action_strength("move_down") \
+		Input.get_action_strength("move_down") \
 	  - Input.get_action_strength("move_up")
 
 	return Vector2(move_x, move_y)
@@ -49,13 +61,21 @@ func on_body_exited(other_body: Node):
 
 func check_deal_damage():
 	if self.colliding_body_count == 0 or \
-		not %DamageIntervalTimer.is_stopped():
+		not self.damage_interval_timer.is_stopped():
 		return
 
-	%HealthComponent.take_damage(1.0)
-	%DamageIntervalTimer.start()
-	print("current health: ", %HealthComponent.current_health)
+	self.health_component.take_damage(1.0)
+	self.damage_interval_timer.start()
+	# print("current health: ", self.health_component.current_health)
+
+
+func update_health_ui():
+	self.health_bar.value = self.health_component.get_health_percent()
 
 
 func on_damage_interval_timer_timeout():
 	self.check_deal_damage()
+
+
+func on_health_changed():
+	self.update_health_ui()

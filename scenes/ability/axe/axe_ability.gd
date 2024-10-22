806adffa9d3696940d0dd636ed30246dcabd2503
yoonsang lee <highp0912@pm.me>
2024-10-23 00:33:44 +0900

    add: axe ability

--- scenes/ability/axe/axe_ability.gd
+++ scenes/ability/axe/axe_ability.gd
@@ -1,2 +1,41 @@
 extends Node2D
 class_name AxeAbility
+
+
+const MAX_RADIUS := 100
+
+@export_range(-5.0, 5.0) var rotations_from: float = 0.0
+@export_range(-5.0, 5.0) var rotations_to: float = 2.0
+@export var duration: float = 3.0
+
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
+
+
+var player: Player
+var base_rotation := Vector2.RIGHT
+
+
+func _ready():
+    self.player = get_tree().get_first_node_in_group("player") as Player
+    if self.player == null:
+        return
+
+    self.base_rotation = Vector2.RIGHT.rotated(randf_range(0, TAU))
+
+    var tween := super.create_tween()
+    tween.tween_method(
+        self.tween_method, \
+        self.rotations_from, \
+        self.rotations_to, \
+        self.duration
+    )
+    tween.tween_callback(self.queue_free)
+
+
+func tween_method(rotations: float):
+    var percent := rotations / 2
+    var current_radius := percent * MAX_RADIUS
+    var current_direction := self.base_rotation.rotated(rotations * TAU)
+
+    global_position = self.player.global_position + \
+        (current_direction * current_radius)
--- scenes/ability/axe/axe_ability_controller.gd
+++ scenes/ability/axe/axe_ability_controller.gd
@@ -2,6 +2,7 @@ extends Node
 class_name AxeAbilityController
 
 @export var axe_ability_scene: PackedScene
+@export var damage: float = 10.0
 
 @onready var timer: Timer = $Timer
 
@@ -16,5 +17,13 @@ func _ready():
 
 
 func spawn_axe():
-    var axe_instance := self.axe_ability_scene.instantiate() as AxeAbility
+    var foreground = get_tree() \
+        .get_first_node_in_group("foreground_layer") as Node2D
+    if foreground == null:
+        return
 
+    var axe_instance := self.axe_ability_scene.instantiate() as AxeAbility
+    foreground.add_child(axe_instance)
+
+    axe_instance.global_position = self.player.global_position
+    axe_instance.hitbox_component.damage = self.damage
